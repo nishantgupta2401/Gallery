e7410198f0d196b7506676c5af3343e06324e92b
unknown <nishantgupta2401@gmail.com>
2019-11-23 18:39:27 +0530

    new changes

--- public/config/config.js
+++ public/config/config.js
@@ -2,8 +2,17 @@
 
 	  
 	var imagesUrl = "http://localhost:3001/";
-	var service_url = "http://localhost:3001/";//Nishant
-	var templateUrl = "http://localhost:3001/";//Nishant
+	var service_url = "http://localhost:3001/";//local
+	var templateUrl = "http://localhost:3001/";//local
+
+	//************End********************************************//
+
+	//************Heroku Configuration********************************//
+
+	  
+	var imagesUrl = "https://sticker-gallery.herokuapp.com/";
+	var service_url = "https://sticker-gallery.herokuapp.com/";//local
+	var templateUrl = "https://sticker-gallery.herokuapp.com/";//local
 
 	//************End********************************************//
 
